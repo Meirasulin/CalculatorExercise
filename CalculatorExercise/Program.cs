e4083e8eea8abf9dee0b9ffef95b498516284222
Meirasulin <asulinmeir64@gmail.com>
2024-10-10 10:06:13 +0300

    Add project files.

---
+++ CalculatorExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
