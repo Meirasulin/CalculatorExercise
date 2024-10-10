606402f9826ec482d42d2f76d8e805b1942dbb7f
Meirasulin <asulinmeir64@gmail.com>
2024-10-10 23:00:07 +0300

    fix: cange the Program class to get one line input of arithmetic expression

--- CalculatorExercise/Program.cs
+++ CalculatorExercise/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CalculatorExercise
 {
@@ -11,54 +8,56 @@ namespace CalculatorExercise
         public static void Main(string[] args)
         {
             Calculator calculator = new Calculator();
-        while (true)
-        {
-            Console.WriteLine("Enter two numbers (or type 'exit' to quit):");
-            string input1 = Console.ReadLine();
-            if (input1.ToLower() == "exit") break;
-            string input2 = Console.ReadLine();
-            if (input2.ToLower() == "exit") break;
-
-            Console.WriteLine("Enter the operation (+, -, *, /):");
-            string operation = Console.ReadLine();
-            if (operation.ToLower() == "exit") break;
-
-            try
+            while (true)
             {
-                double num1 = double.Parse(input1);
-                double num2 = double.Parse(input2);
-                double result = 0;
+                Console.WriteLine("Enter an arithmetic expression (or type 'exit' to quit):");
+                string arithmeticExpression = Console.ReadLine();
+                if (arithmeticExpression.ToLower() == "exit") break;
 
-                switch (operation)
+                try
                 {
-                    case "+":
-                        result = calculator.Add(num1, num2);
-                        break;
-                    case "-":
-                        result = calculator.Subtract(num1, num2);
-                        break;
-                    case "*":
-                        result = calculator.Multiply(num1, num2);
-                        break;
-                    case "/":
-                        result = calculator.Divide(num1, num2);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid operation.");
-                        continue;
-                }
+                    string arithmeticExpressionPattern = @"^(-?\d+\.?\d*)([\+\-\*\/])(-?\d+\.?\d*)$";
+                    Match match = Regex.Match(arithmeticExpression, arithmeticExpressionPattern);
 
-                Console.WriteLine($"The result is: {result}");
+                    if (!match.Success)
+                    {
+                        throw new ArgumentException("Invalid input. Please enter valid arithmetic expression.");
+                    }
+                    double num1 = double.Parse(match.Groups[1].Value);
+                    double num2 = double.Parse(match.Groups[3].Value);
+                    string operation = match.Groups[2].Value;
+                    double result = 0;
+
+                    switch (operation)
+                    {
+                        case "+":
+                            result = calculator.Add(num1, num2);
+                            break;
+                        case "-":
+                            result = calculator.Subtract(num1, num2);
+                            break;
+                        case "*":
+                            result = calculator.Multiply(num1, num2);
+                            break;
+                        case "/":
+                            result = calculator.Divide(num1, num2);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid operation.");
+                            continue;
+                    }
+
+                    Console.WriteLine($"The result is: {result}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-            catch (FormatException)
-            {
-                Console.WriteLine("Invalid input. Please enter numeric values.");
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
         }
     }
 }
