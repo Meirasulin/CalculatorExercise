c62bff9d23a2b578efd9a0395587a5326840645b
Meirasulin <asulinmeir64@gmail.com>
2024-10-10 10:20:59 +0300

    add initial of Calculator class

---
+++ CalculatorExercise/CalculatorExercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExercise
+{
+    internal class Calculator
+    {
+        public double Add(double num1, double num2)
+        {
+            return;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return;
+        }
+        public double Divide(double num1, double num2)
+        {
+            return;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return;
+        }
+    }
+}
